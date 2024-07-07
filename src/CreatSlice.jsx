3a990fd83f40588dd77d80975c27d7bf6f96ea2b
Pahol <pahol.pc@gmail.com>
2024-07-08 01:57:19 +0700

    add functionality

--- src/CreatSlice.jsx
+++ src/CreatSlice.jsx
@@ -1,5 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// type Item {
+//   name
+//   image
+//   cost
+//   quantity
+// }
+
 export const CreatSlice = createSlice({
   name: 'cart',
   initialState: {
@@ -7,12 +14,31 @@ export const CreatSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find((item) => item.name === name);
+
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        const convertedCost = Number(cost.replace('$', ''));
+        state.items.push({
+          name,
+          image,
+          cost: convertedCost,
+          quantity: 1,
+        });
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter((item) => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-    
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find((item) => item.name === name);
+
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
